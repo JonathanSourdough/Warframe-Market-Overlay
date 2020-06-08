041fe946019a0a715edcd0e7e42d197ebd600210
Jonathan Sourdough <github@jsourdough.com>
2020-06-07 20:22:12 -0700

    Changed AWS Lambda code to use passed variables instead of global. Added in an additional page for determining best ducat traders. Other minor tweaks to AWS lambda scripts.

--- LambdaScripts/Lambda_Item_Check.py
+++ LambdaScripts/Lambda_Item_Check.py
@@ -2,36 +2,42 @@ import requests
 import json
 import boto3
 
-API = 'https://api.warframe.market/v1/'
-itemsRaw = {}
-itemsNames = {}
-itemsIds = {}
-itemsDucat = {}
-setsIds = {}
+API = "https://api.warframe.market/v1/"
+
 
 def itemCheck():
-    itemsRaw = requests.get(API + 'items').json()['payload']['items']
+    itemsNames = {}
+    itemsIds = {}
+    setsIds = {}
+    itemsRaw = requests.get(API + "items").json()["payload"]["items"]
     for v in itemsRaw:
-        if (' Prime ' in v['item_name']) and (not ' Set' in v['item_name']):
-            itemsNames[v['item_name']] = v['url_name']
-            itemsIds[v['id']] = v['item_name']
-        elif (' Prime ' in v['item_name']) and (' Set' in v['item_name']):
-            setsIds[v['id']] = v['item_name']
-    itemsRaw = {}
+        if (" Prime " in v["item_name"]) and (not " Set" in v["item_name"]):
+            itemsNames[v["item_name"]] = v["url_name"]
+            itemsIds[v["id"]] = v["item_name"]
+        elif (" Prime " in v["item_name"]) and (" Set" in v["item_name"]):
+            setsIds[v["id"]] = v["item_name"]
+    return itemsNames, itemsIds, setsIds
 
-def ducatCheck():
-    ducatsRaw = requests.get(API + 'tools/ducats').json()['payload']['previous_hour']
+
+def ducatCheck(itemsIds, setsIds):
+    itemsDucat = {}
+    ducatsRaw = requests.get(API + "tools/ducats").json()["payload"]["previous_hour"]
     for i, v in enumerate(ducatsRaw):
-        if (not any(x in v['item'] for x in setsIds)):
-            itemsDucat[itemsIds[v['item']]] = v['ducats']
-    ducatsRaw = {}
+        if not any(x in v["item"] for x in setsIds):
+            itemsDucat[itemsIds[v["item"]]] = v["ducats"]
+    return itemsDucat
+
 
 def lambda_handler(event, context):
-    print('Started item and ducat check')
-    itemCheck()
-    ducatCheck()
-    ItemList = {'itemsNames': itemsNames, 'itemsDucat': itemsDucat}
-    client = boto3.client('s3')
-    client.delete_objects(Bucket='www.jsourdough.com', Delete={'Objects': [{'Key': 'ItemsAndDucats.json'},]})
-    client.put_object(Bucket='www.jsourdough.com', Key='ItemsAndDucats.json', Body=json.dumps(ItemList, indent=4), ContentType='text/plain')
-    print('Page Updated')
+    print("Started item and ducat check")
+    itemsNames, itemsIds, setsIds = itemCheck()
+    itemsDucat = ducatCheck(itemsIds, setsIds)
+    ItemList = {"itemsNames": itemsNames, "itemsDucat": itemsDucat}
+    client = boto3.client("s3")
+    client.put_object(
+        Bucket="www.jsourdough.com",
+        Key="ItemsAndDucats.json",
+        Body=json.dumps(ItemList, indent=4),
+        ContentType="text/plain",
+    )
+    print("Page Updated")
--- LambdaScripts/Lambda_Plat_Check.py
+++ LambdaScripts/Lambda_Plat_Check.py
@@ -6,39 +6,27 @@ import operator
 import boto3
 
 API = "https://api.warframe.market/v1/items"
-itemsNames = {}
-itemsOrdersingame = {}
-itemsOrdersingame["sell"] = {}
-itemsOrdersingame["buy"] = {}
-itemsOrderstopTen = {}
-itemsOrderstopTen["sell"] = {}
-itemsOrderstopTen["buy"] = {}
-itemsOrderscheapest = {}
-itemsOrderscheapest["sell"] = {}
-itemsOrderscheapest["buy"] = {}
-itemsOrdersaverage = {}
-itemsOrdersaverage["sell"] = {}
-itemsOrdersaverage["buy"] = {}
-itemsDucat = {}
-ducatPlatledgerUnsorted = {}
-ducatPlatledgerSorted = []
 
 
 def importNamesandDucats():
     imported = requests.get(
         "https://s3.us-east-2.amazonaws.com/www.jsourdough.com/ItemsAndDucats.json"
     ).json()
-    global itemsNames
-    global itemsDucat
     itemsNames = imported["itemsNames"]
     itemsDucat = imported["itemsDucat"]
+    return itemsNames, itemsDucat
 
 
-def updateItemplat(itemName):
-    global itemsOrdersingame
-    global itemsOrderstopTen
-    global itemsOrderscheapest
-    global itemsOrdersaverage
+def updateItemplat(
+    itemName,
+    itemsNames,
+    itemsDucat,
+    itemsOrderstopTen,
+    itemsOrderscheapest,
+    itemsOrdersaverage,
+    playerListings,
+):
+    itemsOrdersingame = {"sell": {}, "buy": {}}
     itemRaw = {}
     itemRaw = requests.get(API + "/" + itemsNames[itemName] + "/orders").json()["payload"]["orders"]
     itemsOrdersingame["sell"][itemName] = {}
@@ -53,20 +41,6 @@ def updateItemplat(itemName):
                 itemsOrdersingame["buy"][itemName][itemRaw[i]["user"]["ingame_name"]] = itemRaw[i][
                     "platinum"
                 ]
-    #### Sell
-    if itemsOrdersingame["sell"][itemName] == {}:
-        itemsOrdersingame["sell"][itemName] = {"No sellers": 0}
-    itemsOrderstopTen["sell"][itemName] = []
-    itemsOrderstopTen["sell"][itemName] = sorted(
-        itemsOrdersingame["sell"][itemName].items(), key=operator.itemgetter(1)
-    )
-    while len(itemsOrderstopTen["sell"][itemName]) >= 11:
-        itemsOrderstopTen["sell"][itemName].pop()
-    itemsOrderscheapest["sell"][itemName] = itemsOrderstopTen["sell"][itemName][0]
-    average = 0
-    for i, tk in enumerate(itemsOrderstopTen["sell"][itemName]):
-        average += itemsOrderstopTen["sell"][itemName][i][1]
-        itemsOrdersaverage["sell"][itemName] = average / len(itemsOrderstopTen["sell"][itemName])
     #### Buy
     if itemsOrdersingame["buy"][itemName] == {}:
         itemsOrdersingame["buy"][itemName] = {"No buyers": 0}
@@ -82,26 +56,84 @@ def updateItemplat(itemName):
     for i, tk in enumerate(itemsOrderstopTen["buy"][itemName]):
         average += itemsOrderstopTen["buy"][itemName][i][1]
         itemsOrdersaverage["buy"][itemName] = average / len(itemsOrderstopTen["buy"][itemName])
-    itemsOrdersingame["sell"].pop(itemName)
-    itemsOrdersingame["buy"].pop(itemName)
+    #### Sell
+    if itemsOrdersingame["sell"][itemName] == {}:
+        itemsOrdersingame["sell"][itemName] = {"No sellers": 0}
+    itemsOrderstopTen["sell"][itemName] = []
+    itemsOrderstopTen["sell"][itemName] = sorted(
+        itemsOrdersingame["sell"][itemName].items(), key=operator.itemgetter(1)
+    )
+    while len(itemsOrderstopTen["sell"][itemName]) >= 11:
+        itemsOrderstopTen["sell"][itemName].pop()
+    itemsOrderscheapest["sell"][itemName] = itemsOrderstopTen["sell"][itemName][0]
+    average = 0
+    for i, tk in enumerate(itemsOrderstopTen["sell"][itemName]):
+        average += itemsOrderstopTen["sell"][itemName][i][1]
+        itemsOrdersaverage["sell"][itemName] = average / len(itemsOrderstopTen["sell"][itemName])
+    # player listings
+    for k, v in itemsOrdersingame["sell"][itemName].items():
+        if not k in playerListings:
+            playerListings[k] = {
+                "playerName": k,
+                "playerPlat": 0,
+                "playerDucats": 0,
+                "playerEfficency": 0,
+                "unorderedItems": [],
+                "orderedItems": [],
+            }
+        for v2 in playerListings[k]["unorderedItems"]:
+            if itemName in v2:
+                if v2["platinum"] < v:
+                    continue
+        try:
+            playerListings[k]["unorderedItems"].append(
+                {"platinum": v, "itemName": itemName, "ducats": itemsDucat[itemName]}
+            )
+        except KeyError:
+            playerListings[k]["unorderedItems"].append(
+                {"platinum": v, "itemName": itemName, "ducats": 0}
+            )
+    return (itemsOrderstopTen, itemsOrderscheapest, itemsOrdersaverage, playerListings)
 
 
-def platCheck():
+def platCheck(itemsNames, itemsDucat):
+    itemsOrderstopTen = {"sell": {}, "buy": {}}
+    itemsOrderscheapest = {"sell": {}, "buy": {}}
+    itemsOrdersaverage = {"sell": {}, "buy": {}}
+    playerListings = {}
     iteration = 0
     for k, tv in itemsNames.items():
         start_time = time.time()
-        updateItemplat(k)
+        stuff = updateItemplat(
+            k,
+            itemsNames,
+            itemsDucat,
+            itemsOrderstopTen,
+            itemsOrderscheapest,
+            itemsOrdersaverage,
+            playerListings,
+        )
+        itemsOrderstopTen.update(stuff[0])
+        itemsOrderscheapest.update(stuff[1])
+        itemsOrdersaverage.update(stuff[2])
+        playerListings.update(stuff[3])
         iteration += 1
         if (time.time() - start_time) <= 0.34:
             sleep = 0.34 - (time.time() - start_time)
             if sleep < 0.01:
                 sleep = 0.01
             time.sleep(sleep)
-        print("[{}/{}]".format(iteration, len(itemsNames.items())))
+        # print("[{}/{}]".format(iteration, len(itemsNames.items())))
     itemsOrdersraw = {}
+    return (
+        itemsOrderstopTen,
+        itemsOrderscheapest,
+        itemsOrdersaverage,
+        playerListings,
+    )
 
 
-def ducatPlatefficency(itemName):
+def ducatPlatefficency(itemName, itemsDucat, itemsOrderscheapest):
     try:
         return itemsDucat[itemName] / itemsOrderscheapest["sell"][itemName][1]
     except ZeroDivisionError:
@@ -110,21 +142,19 @@ def ducatPlatefficency(itemName):
         return "No ducat value listed on Warframe.Market, please notify Warframe.Market"
 
 
-def bestDucatplatEfficencycalc():
-    global ducatPlatledgerUnsorted
-    global ducatPlatledgerSorted
+def bestDucatplatEfficencycalc(itemsNames, itemsDucat, itemsOrderscheapest):
+    ducatPlatledgerSorted = []
+    ducatPlatledgerUnsorted = {}
     for k, v in itemsNames.items():
-        add = ducatPlatefficency(k)
+        add = ducatPlatefficency(k, itemsDucat, itemsOrderscheapest)
         if add != "No ducat value listed on Warframe.Market, please notify Warframe.Market":
             ducatPlatledgerUnsorted[k] = add
-        ducatPlatledgerSorted = sorted(ducatPlatledgerUnsorted.items(), key=operator.itemgetter(1))
+    ducatPlatledgerSorted = sorted(ducatPlatledgerUnsorted.items(), key=operator.itemgetter(1))
     ducatPlatledgerUnsorted = {}
+    return ducatPlatledgerSorted
 
 
-def bestDucatplatEfficencytxt(count):
-    global itemsOrderscheapest
-    global itemsDucat
-    global ducatPlatledgerSorted
+def bestDucatplatEfficencytxt(count, itemsDucat, itemsOrderscheapest, ducatPlatledgerSorted):
     bestDucatplatEfficencytxtList = []
     for i in range(1, count + 1):
         bestDucatplatEfficencytxtList.append(
@@ -141,18 +171,60 @@ def bestDucatplatEfficencytxt(count):
             + str(ducatPlatledgerSorted[-i][1])
             + "\n"
         )
-    return str(datetime.datetime.now()) + "\n\n" + "\n".join(bestDucatplatEfficencytxtList)
+    return (
+        str(datetime.datetime.isoformat(datetime.datetime.now()))
+        + "\n\n"
+        + "\n".join(bestDucatplatEfficencytxtList)
+    )
+
+
+def playerListingsaccumulator(playerListings):
+    for k, v in playerListings.items():
+        for v2 in v["unorderedItems"]:
+            try:
+                v2["efficency"] = v2["ducats"] / v2["platinum"]
+            except ZeroDivisionError:
+                v2["efficency"] = 0
+        playerSalessorted = sorted(v["unorderedItems"], key=operator.itemgetter("efficency"))
+        playerSalessorted.reverse()
+        for v2 in playerSalessorted:
+            for v3 in v["unorderedItems"]:
+                if v2["itemName"] in v3.values():
+                    v["orderedItems"].append(v3)
+            if len(v["orderedItems"]) >= 6:
+                break
+        del playerSalessorted
+        del v["unorderedItems"]
+        for v2 in v["orderedItems"]:
+            v["playerPlat"] = v["playerPlat"] + v2["platinum"]
+            v["playerDucats"] = v["playerDucats"] + v2["ducats"]
+        try:
+            v["playerEfficency"] = v["playerDucats"] / v["playerPlat"]
+        except ZeroDivisionError:
+            v["playerEfficency"] = 0
+    playerDucatefficencysorted = sorted(
+        playerListings.values(), key=operator.itemgetter("playerEfficency")
+    )
+    playerDucatefficencysorted.reverse()
+    json = {
+        "last Updated": str(datetime.datetime.isoformat(datetime.datetime.now())),
+        "playersByefficency": playerDucatefficencysorted,
+    }
+    return json
 
 
 def lambda_handler(event, context):
-    print("Started plat and efficency checks", datetime.datetime.now())
-    importNamesandDucats()
-    print("Finished name and ducat import", datetime.datetime.now())
-    platCheck()
-    print("Finished plat check", datetime.datetime.now())
-    bestDucatplatEfficencycalc()
-    print("Finished efficency check", datetime.datetime.now())
+    print("Started plat and efficency checks", datetime.datetime.isoformat(datetime.datetime.now()))
+    itemsNames, itemsDucat = importNamesandDucats()
+    print("Finished name and ducat import", datetime.datetime.isoformat(datetime.datetime.now()))
+    itemsOrderstopTen, itemsOrderscheapest, itemsOrdersaverage, playerListings = platCheck(
+        itemsNames, itemsDucat
+    )
+    print("Finished plat check", datetime.datetime.isoformat(datetime.datetime.now()))
+    ducatPlatledgerSorted = bestDucatplatEfficencycalc(itemsNames, itemsDucat, itemsOrderscheapest)
+    print("Finished efficency check", datetime.datetime.isoformat(datetime.datetime.now()))
     toClient = {
+        "last Updated": str(datetime.datetime.isoformat(datetime.datetime.now())),
         "itemsNames": itemsNames,
         "itemsOrderstopTen": itemsOrderstopTen,
         "itemsOrderscheapest": itemsOrderscheapest,
@@ -161,22 +233,22 @@ def lambda_handler(event, context):
         "ducatPlatledgerSorted": ducatPlatledgerSorted,
     }
     client = boto3.client("s3")
-    client.delete_objects(
-        Bucket="www.jsourdough.com", Delete={"Objects": [{"Key": "ImportantInfo.json"}]}
-    )
     client.put_object(
         Bucket="www.jsourdough.com",
         Key="ImportantInfo.json",
         Body=json.dumps(toClient, indent=4),
         ContentType="text/plain",
     )
-    client.delete_objects(
-        Bucket="www.jsourdough.com", Delete={"Objects": [{"Key": "bestDucatplatEfficency.txt"}]}
-    )
     client.put_object(
         Bucket="www.jsourdough.com",
         Key="bestDucatplatEfficency.txt",
-        Body=bestDucatplatEfficencytxt(5),
+        Body=bestDucatplatEfficencytxt(20, itemsDucat, itemsOrderscheapest, ducatPlatledgerSorted),
         ContentType="text/plain",
     )
-    print("Pages Updated", datetime.datetime.now())
+    client.put_object(
+        Bucket="www.jsourdough.com",
+        Key="playerListings.json",
+        Body=json.dumps(playerListingsaccumulator(playerListings), indent=4),
+        ContentType="text/plain",
+    )
+    print("Pages Updated", datetime.datetime.isoformat(datetime.datetime.now()))
